de5589bcda80c9ea72434758a50c298bdca3cdd3
Justin Bass <justinbass6036@gmail.com>
2023-02-06 23:54:54 -0500

    commit message

--- src/components/VinylList.js
+++ src/components/VinylList.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Vinyl from "./Vinyl";
 
 function VinylList({vinylData}){
+    const [lessThanTwenty, setLessThanTwenty] = useState(false)
+
     let onlyNewVinylsToDispay = vinylData.slice(0, 6)
+    .filter((vinly) => lessThanTwenty ? vinly.price < 20 : vinly.price)
+
+    function handleFilterLessThanClick(){
+        setLessThanTwenty((lessThanTwenty) => !lessThanTwenty)
+    }
+
+
     return(
         <>
+        <div>
+        <button
+        className="button-black"
+        onClick={handleFilterLessThanClick}
+        >Vinyls Less Than $20
+        </button>
+        </div>
+
         {onlyNewVinylsToDispay.map((vinyl) => (
             <Vinyl
             key={vinyl.id}
